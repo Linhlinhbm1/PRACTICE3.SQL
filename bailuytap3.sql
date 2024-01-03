7c6055a7733aa40f67a1751bc8326eea7c76e3d3
Linhlinhbm1 <153344152+Linhlinhbm1@users.noreply.github.com>
2024-01-03 17:37:57 +0700

    Update bailuytap3.sql

--- bailuytap3.sql
+++ bailuytap3.sql
@@ -24,3 +24,30 @@ FROM reviews
 GROUP BY EXTRACT( month from submit_date ),product_id
 ORDER BY EXTRACT( month from submit_date ), product_id
 --baitap5--
+SELECT  sender_id,
+ count(message_id)
+FROM messages 
+where EXTRACT(month from sent_date) = 8
+and EXTRACT(year from sent_date) = 2022
+group by sender_id
+ORDER BY count(message_id) DESC
+limit 2
+--baitap6--
+select tweet_id from Tweets 
+where length(content) > 15    
+--baitap8--
+select 
+count (id)
+from employees
+where extract (month from joining_date) between 1 and 7
+and extract (year from joining_date) = 2022
+--baitap9--
+select position( 'a' in first_name)
+from worker
+where first_name = 'Amitah'
+--baitap10--
+select substring (title from  position ('2' in title) for 4)
+from winemag_p2
+where country ='Macedonia'
+
+
